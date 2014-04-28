0bd74fdfc974397b8abbf42fb888125ec1dcd593
Koji Katayama <katayama@randmax.jp>
2014-04-28 09:48:23 +0900

    課題9まで終了
    課題10は後回し

---
+++ ex02.v
@@ -0,0 +1,143 @@
+Module Ex2_7.
+
+Require Import Arith.
+
+(* Problem 6 *)
+
+Goal forall x y, x < y -> x + 10 < y + 10.
+Proof.
+  intros.
+  SearchAbout (_ + _ < _ + _).
+  apply plus_lt_compat_r.
+  apply H.
+Qed.
+
+
+(* Problem 7 *)
+Goal forall P Q : nat -> Prop, P 0 -> (forall x, P x -> Q x) -> Q 0.
+Proof.
+  intros.
+  apply (H0 0).
+  apply H.
+Qed.
+
+
+Goal forall P : nat -> Prop, P 2 -> (exists y, P (1 + y)).
+Proof.
+  intros.
+  exists 1.
+  apply H.
+Qed.
+
+
+Goal forall P : nat -> Prop, (forall n m, P n -> P m) -> (exists p, P p) -> forall q, P q.
+Proof.
+  intros.
+  destruct H0.
+  apply (H x q).
+  apply H0.
+Qed.
+
+End Ex2_7.
+
+Module Ex2_8.
+Require Import Arith.
+
+Goal forall m n : nat, (n * 10) + m = (10 * n) + m.
+Proof.
+  intros.
+  apply NPeano.Nat.add_cancel_r.
+  apply mult_comm.
+Qed.
+
+End Ex2_8.
+
+Module Ex2_9.
+
+Require Import Arith.
+
+Goal forall n m p q : nat, (n + m) + (p + q) = (n + p) + (m + q).
+Proof.
+  intros.
+  SearchAbout (_+(_+_)).
+  rewrite plus_assoc.
+  rewrite plus_assoc.
+  SearchAbout (_+_=_+_).
+  apply NPeano.Nat.add_cancel_r.
+  rewrite <- plus_assoc.
+  rewrite <- plus_assoc.
+  apply NPeano.Nat.add_cancel_l.
+  apply plus_comm.
+Qed.
+
+
+Goal forall n m : nat, (n + m) * (n + m) = n * n + m * m + 2 * n * m.
+Proof.
+  intros.
+  SearchAbout ((_+_)*_).
+  rewrite NPeano.Nat.mul_add_distr_r.
+  rewrite NPeano.Nat.mul_add_distr_l.
+  rewrite NPeano.Nat.mul_add_distr_l.
+  rewrite <- plus_assoc.
+  rewrite <- plus_assoc.
+  apply NPeano.Nat.add_cancel_l.
+  rewrite plus_assoc.
+  rewrite plus_comm.
+  apply NPeano.Nat.add_cancel_l.
+  simpl.
+  SearchAbout (_ + 0).
+  rewrite plus_0_r.
+  rewrite mult_comm.
+  rewrite mult_comm.
+  SearchAbout ((_+_)*_).  
+  rewrite mult_plus_distr_r.
+  reflexivity.
+Qed.
+
+End Ex2_9.
+
+Module Ex2_10.
+
+Parameter G : Set.
+Parameter mult : G -> G -> G.
+Notation "x * y" := (mult x y).
+Parameter one : G.
+Notation "1" := one.
+Parameter inv : G -> G.
+Notation "/ x" := (inv x).
+(* Notation "x / y" := (mult x (inv y)). *) (* 使ってもよい *)
+
+Axiom mult_assoc : forall x y z, x * (y * z) = (x * y) * z.
+Axiom one_unit_l : forall x, 1 * x = x.
+Axiom inv_l : forall x, /x * x = 1.
+
+
+
+Lemma inv_r : forall x, x * / x = 1.
+Proof.
+  intros.
+  rewrite <- (one_unit_l (x * / x)).
+  rewrite -> mult_assoc.
+  rewrite <- (inv_l 1).
+
+  rewrite <- (one_unit_l x).
+  rewrite mult_assoc.
+
+  rewrite <- (one_unit_l x).
+  rewrite -> one_unit_l.
+  rewrite -> one_unit_l.
+  reflexivity.
+
+  assert (forall x, /x * x = 1) as H1.
+  apply inv_l.
+  rewrite <- one_unit_l in H1.
+  rewrite mult_assoc.
+  rewrite one_unit_l.
+  rewrite inv_l.
+  rewrite <- one_unit_l.
+
+
+
+
+
+
